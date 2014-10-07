b679f00897460fe4f3af9198317839913a64ba6d
wdi-sf-fall <markus.guehrs@generalassemb.ly>
2014-10-07 14:15:23 -0700

    solution

--- src/rental_property/building.js
+++ src/rental_property/building.js
@@ -1,42 +1,58 @@
 "use strict"
 
 function Building(address) {
-  // building has an address
-  // ...
-  // and array of units
-  // ...
+  this.address = address;
+  this.units = [];
 }
 
 Building.prototype.setManager = function(person) {
-  // set this.manager to person. Person needs to be of type Manager
-  // ...
+  // set this.manager to person
+
+  if(person.constructor.name === "Manager"){
+    this.manager = person;
+  }
+
+  return this;
 };
 
 Building.prototype.getManager = function(){
   // return this.manager 
-  // ..
+  return this.manager;
 };
 
 Building.prototype.addTenant = function(unit, tenant) {
   // add tenant but check to make sure there
   // is a manager first and a tenant has 2 references
-  // Note that tenenat does not belong to Building, but to Unit
-  // ...
+  var validUnit = this.units.indexOf(unit) !== -1 && unit.available();
+  var validTenant = tenant.references.length >= 2 && tenant.constructor.name === "Tenant";
+
+  if(this.manager && validUnit && validTenant){
+    unit.tenant = tenant;
+  }
 };
 
 Building.prototype.removeTenant = function(unit, tenant) {
   // remove tenant
-  // ...
+  var validUnit = this.units.indexOf(unit) !== -1;
+  var validTenant = unit.tenant === tenant;
+  if(this.manager && validUnit && validTenant){
+    unit.tenant = null;
+  }
 };
 
 Building.prototype.availableUnits = function(){
   // return units available
-  // ...
+  return this.units.filter(function(unit){
+    return unit.available();
+  });
 };
 
 Building.prototype.rentedUnits = function(){
   // return rented units
-  // ...
+  return this.units.filter(function(unit){
+    return !unit.available();
+  });
 };
+
 
 module.exports = Building;
